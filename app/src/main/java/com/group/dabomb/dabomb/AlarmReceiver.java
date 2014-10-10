656245999503639451f225005f0e590b40f39baf
Stefan <sdneamtu@gmail.com>
2014-10-10 14:14:24 -0400

    Fixed issues

--- app/src/main/java/com/group/dabomb/dabomb/AlarmReceiver.java
+++ app/src/main/java/com/group/dabomb/dabomb/AlarmReceiver.java
@@ -1,7 +1,12 @@
 package com.group.dabomb.dabomb;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 
 /**
  * Created by Stefan on 10/10/2014.
@@ -11,16 +16,16 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent){
         long id = intent.getLongExtra("id",0);
-        String message = intent.getStringExtrea("message");
+        String message = intent.getStringExtra("message");
 
         Notification notification = new Notification(R.drawable.ic_launcher,message,System.currentTimeMillis());
 
         PendingIntent pend = PendingIntent.getActivity(context,0,new Intent(),0);
 
         notification.setLatestEventInfo(context,"Alarm", message, pend);
-        notification.sound = Uri.parse(RemindMe.getRingtone());
+       // notification.sound
 
-        NotificationManager notManager = (NotificationManager) context.getSystemSerivce(Context.NOTIFICATION_SERVICE);
+        NotificationManager notManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         notManager.notify((int)id, notification);
     }

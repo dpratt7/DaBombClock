7efe47c08592bf2ccfe375c36941b2c77b30177c
Stefan <sdneamtu@gmail.com>
2014-10-15 14:42:45 -0400

    AlarmSetter recreates alarm upon boot

---
+++ app/src/main/java/com/group/dabomb/dabomb/AlarmSetter.java
@@ -0,0 +1,20 @@
+package com.group.dabomb.dabomb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Stefan on 10/15/2014.
+ */
+public class AlarmSetter extends BroadcastReceiver {
+
+    @Override
+    public void onRecieve(Context context, Intent intent){
+        Intent setter = new Intent(context,AlarmService.class);
+        setter.setAction((AlarmService.Create));
+        context.startService(setter);
+
+    }
+
+}

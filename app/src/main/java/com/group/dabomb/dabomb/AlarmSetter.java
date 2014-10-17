c891e5fb296b4fbe60462e4b6e6964801d288453
Stefan <sdneamtu@gmail.com>
2014-10-17 14:13:14 -0400

    Fixed AlarmSetter

--- app/src/main/java/com/group/dabomb/dabomb/AlarmSetter.java
+++ app/src/main/java/com/group/dabomb/dabomb/AlarmSetter.java
@@ -12,9 +12,8 @@ public class AlarmSetter extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent){
         Intent setter = new Intent(context,AlarmService.class);
-        setter.setAction((AlarmService.Create));
+        //setter.setAction((AlarmService.CREATE));
         context.startService(setter);
 
     }
-
 }
